fe13e08333d3649b99e24cfb0f0fc0943853d6ac
Mirlan <mirlan.jelamanov@gmail.com>
2019-09-30 22:27:56 +0600

    Adds exceptions information

--- src/Rule/RuleFactoryInterface.php
+++ src/Rule/RuleFactoryInterface.php
@@ -15,6 +15,9 @@ interface RuleFactoryInterface
      * @param string|null $guard
      *
      * @return \Illuminate\Contracts\Validation\Rule
+     *
+     * @throws \InvalidArgumentException If a guard is not registered in the application.
+     * @throws \Mjelamanov\Laravel\AuthPassword\Exception\RuleCreationFailedException Whenever it cannot instantiate a rule.
      */
     public function createRule(?string $guard = null): Rule;
 }
